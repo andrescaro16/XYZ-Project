cb7caa1c84f9b6b5fee1c949b9dabe0a0a5a7be1
andrescaro16 <julicaro2003@gmail.com>
2023-07-21 16:42:41 -0500

    Add frontend base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <p className='text-5xl font-semibold'> XYZ Project </p>
+      </div>
+    </>
+  )
+}
+
+export default App
